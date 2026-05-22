a8c35690fce3d446cc44b3b86db7755845d23756
AoT-Eren_Yeager <sergejfilenko59@gmail.com>
2026-05-22 18:26:46 +0200

    Connect login.html to express

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 4000;
 const path = require('path');
 
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
 });
